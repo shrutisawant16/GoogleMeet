f94c731c64535a5ceda9a834bb405f56f7e72ba7
shruti <shrutisawant162003@gmail.com>
2023-12-14 16:50:17 +0530

    hii go

---
+++ mernbackend/src/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/",(req, res)=>{
+res.send("hello from the shruti sawant")
+});
+
+app.listen(port,()=>{
+console.log('server is running at port no ${port}');
+})
